926203d83f6aeaf74d9947e9d33226e44f33ec22
Johann Schmitz <johann@j-schmitz.net>
2015-10-26 08:53:05 +0100

    default to nagios/icinga-compatible output

--- src/smartcheck/main.py
+++ src/smartcheck/main.py
@@ -2,7 +2,7 @@
 from argparse import ArgumentParser
 import os
 import sys
-from smartcheck.check import SMARTCheck
+from smartcheck.check import SMARTCheck, AttributeWarning
 
 DEFAULT_DATA_FILE=os.path.join(os.path.dirname(__file__), 'disks.yaml')
 
@@ -10,6 +10,8 @@ if __name__ == "__main__":
 	parser = ArgumentParser()
 	parser.add_argument('--data-file', default=DEFAULT_DATA_FILE)
 	parser.add_argument('-f', '--file', help="Use S.M.A.R.T. report from file instead of calling smartctl (Use - to read from stdin)")
+	parser.add_argument('-x', '--exclude-notices', help='Report NOTICE warnings (default: %(default)s)', action='store_true', default=False)
+	parser.add_argument('-v', '--verbose', help='Verbose messages', action='store_true', default=False)
 
 	args = parser.parse_args()
 
@@ -22,15 +24,27 @@ if __name__ == "__main__":
 
 	check = SMARTCheck(stream, args.data_file)
 
-	import pprint
-	print(check.information)
-	#print(check.device_model, check.exists_in_database())
-	#print(check.check())
-	print(check.check_tests())
+	exit_code = 0
+	msg = ""
 
-	#pprint.pprint(check.smart_data)
-	#pprint.pprint(check.self_tests)
+	attribute_errors = check.check_attributes()
 
-	#if not check.check_attributes():
-	#	import sys
-	#	sys.exit(1)
+	if args.exclude_notices:
+		for k in [x for x, y in attribute_errors.items() if y.level == AttributeWarning.Notice]:
+			del attribute_errors[k]
+
+	if any((ae.level == AttributeWarning.Warning for ae in attribute_errors.values())):
+		msg = ', '.join([ae.long_message if args.verbose else ae.short_message for ae in attribute_errors.values()])
+		exit_code = 1
+	if any((ae.level == AttributeWarning.Critical for ae in attribute_errors.values())):
+		msg = ', '.join([ae.long_message if args.verbose else ae.short_message for ae in attribute_errors.values()])
+		exit_code = 2
+	if not check.check_tests():
+		msg = (msg + '; S.M.A.R.T. self test reported an error').lstrip(';').strip()
+		exit_code = 2
+
+	if not exit_code:
+		msg = "S.M.A.R.T. data OK"
+
+	print(msg)
+	sys.exit(exit_code)
