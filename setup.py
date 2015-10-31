033e4701963331f9560083c33be9b0f81c39db52
Johann Schmitz <johann@j-schmitz.net>
2015-10-31 07:55:52 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+	name='smart-check',
+	version='0.1',
+	description='A smart S.M.A.R.T. check',
+	author='Johann Schmitz',
+	author_email='johann@j-schmitz.net',
+	url='https://ercpe.de/projects/smart-check',
+	download_url='https://code.not-your-server.de/smart-check.git/tags/',
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	include_package_data=True,
+	package_data = {'': ['*.yaml']},
+	zip_safe=False,
+	license='GPL-3',
+)
